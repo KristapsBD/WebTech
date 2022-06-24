2483c21aab25d0ca92a6a36e12571c82ce9c514d
Kristaps Briks <kristaps.briks@inbox.lv>
2022-06-24 14:02:12 +0300

    Fixed cart total
    Cart total displays correct value with respect to item quantities

--- resources/views/user/showcart.blade.php
+++ resources/views/user/showcart.blade.php
@@ -1,66 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
 
-  @include('user.css')
+    @include('user.css')
 
-  </head>
+</head>
 
-  <body>
+<body>
 
     @include('user.header')
 
     <div style="padding: 100px;">
         <table class="table table-bordered">
-        <thead class="thead-dark">
-          <tr>
-              <th class="p-3">Product name</th>
-              <th class="p-3">Quantity</th>
-              <th class="p-3">Price</th>
-              <th class="p-3 w-10">Edit</th>
-          </tr>
-          <form action="{{ url('order') }}" method="post">
-          @csrf
-          @php
-              $total = 0
-          @endphp
-          @foreach ($cart as $item)
-          <tr>
-              <td>
-                {{ $item->product_title }}
-                <input type="text" name="productname[]" value="{{ $item->product_title }}" hidden>
-              </td>
-              <td>
-                {{ $item->quantity }}
-                <input type="text" name="quantity[]" value="{{ $item->quantity }}" hidden>
-              </td>
-              <td>
-                ${{ $item->price }}
-                <input type="text" name="price[]" value=" ${{ $item->price }}" hidden>
-              </td>
-              <td><a class="btn btn-danger" href="{{ url('delete', $item->id) }}">Remove</a></td>
-              @php
-                  $total += $item->price
-              @endphp
-          </tr>
-          @endforeach
-          <tr>
-              <td colspan="2" class="bg-light">Total</td>
-              <td colspan="2" class="bg-light">${{ $total }}</td>
-          <tr>
+            <thead class="thead-dark">
+                <tr>
+                    <th class="p-3">Product name</th>
+                    <th class="p-3">Quantity</th>
+                    <th class="p-3">Price</th>
+                    <th class="p-3 w-10">Edit</th>
+                </tr>
+                <form action="{{ url('order') }}" method="post">
+                    @csrf
+                    @php
+                    $total = 0
+                    @endphp
+                    @foreach ($cart as $item)
+                    <tr>
+                        <td>
+                            {{ $item->product_title }}
+                            <input type="text" name="productname[]" value="{{ $item->product_title }}" hidden>
+                        </td>
+                        <td>
+                            {{ $item->quantity }}
+                            <input type="text" name="quantity[]" value="{{ $item->quantity }}" hidden>
+                        </td>
+                        <td>
+                            ${{ $item->price }}
+                            <input type="text" name="price[]" value="{{ $item->price }}" hidden>
+                        </td>
+                        <td><a class="btn btn-danger" href="{{ url('delete', $item->id) }}">Remove</a></td>
+                        @php
+                        $quantity = $item->quantity
+                        @endphp
+                        @for ($i = $quantity; $i > 0; $i--)
+                          @php
+                            $total += $item->price
+                          @endphp
+                        @endfor
+                    </tr>
+                    @endforeach
+                    <tr>
+                        <td colspan="2" class="bg-light">Total</td>
+                        <td colspan="2" class="bg-light">${{ $total }}</td>
+                    <tr>
         </table>
-              <!-- Add hacker protection -->
-              @if($total != 0)
-                <div>
-                    <button class="btn btn-primary float-right">Confirm order</button>
-                </div>
-              @endif
+        <!-- Add hacker protection -->
+        @if($total != 0)
+        <div>
+            <button class="btn btn-primary float-right">Confirm order</button>
+        </div>
+        @endif
         </form>
-      </div>
+    </div>
 
     @include('user.scripts')
 
-  </body>
+</body>
 
 </html>
