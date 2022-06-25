82691eae82160af5a23d3d78330aa9d92ab43092
Kristaps Briks <kristaps.briks@inbox.lv>
2022-06-25 20:08:58 +0300

    Basic DB seeder implementation
    Implemented seeder for users, products, orders and reviews tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,83 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $users = [
+            ['name' => 'user',
+            'email' => 'user@gmail.com',
+            'usertype' => 0,
+            'phone' => '+37120289000',
+            'address' => 'Iecavas street 5',
+            'password' => Hash::make('password123')],
+            ['name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'usertype' => 1,
+            'phone' => '+37120289000',
+            'address' => 'Iecavas street 5',
+            'password' => Hash::make('password123')],
+        ];
+        DB::table('users')->insert($users);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $products = [
+            ['title' => 'Summer shirt',
+            'price' => 20,
+            'description' => 'Perfect blend of form and function',
+            'quantity' => 10,
+            'image' => '1655632462.jpg'],
+            ['title' => 'Jeans',
+            'price' => 15,
+            'description' => 'Comfortable jeans for warm summer nights',
+            'quantity' => 7,
+            'image' => '1655990584.jpg'],
+            ['title' => 'Shorts',
+            'price' => 10,
+            'description' => 'Cool looking shorts',
+            'quantity' => 5,
+            'image' => '1655990640.jpg'],
+        ];
+        DB::table('products')->insert($products);
+
+        $orders = [
+            ['name' => 'user',
+            'phone' => '+37120289000',
+            'address' => 'Iecavas street 5',
+            'product_id' => '1',
+            'product_name' => 'Summer shirt',
+            'quantity' => 1,
+            'price' => 20,
+            'status' => 'Not delivered',
+            ],
+            ['name' => 'user',
+            'phone' => '+37120289000',
+            'address' => 'Iecavas street 5',
+            'product_id' => '2',
+            'product_name' => 'Jeans',
+            'quantity' => 1,
+            'price' => 15,
+            'status' => 'Not delivered',
+            ],
+            ['name' => 'user',
+            'phone' => '+37120289000',
+            'address' => 'Iecavas street 5',
+            'product_id' => '3',
+            'product_name' => 'Shorts',
+            'quantity' => 1,
+            'price' => 10,
+            'status' => 'Not delivered',
+            ],
+        ];
+        DB::table('orders')->insert($orders);
+
+        $reviews = [
+            ['user_id' => '1',
+            'prod_id' => '1',
+            'stars' => 3],
+            ['user_id' => '1',
+            'prod_id' => '2',
+            'stars' => 4],
+            ['user_id' => '1',
+            'prod_id' => '3',
+            'stars' => 5],
+        ];
+        DB::table('reviews')->insert($reviews);
     }
 }
