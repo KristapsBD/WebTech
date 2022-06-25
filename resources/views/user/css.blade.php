9ce379269c28c48d6c97b90449241e193945ab7b
Kristaps Briks <kristaps.briks@inbox.lv>
2022-06-25 19:34:29 +0300

    Clean up
    Changed file names for easier use

--- resources/views/user/css.blade.php
+++ resources/views/user/css.blade.php
@@ -14,11 +14,11 @@
 
 <!-- Additional CSS Files -->
 <link rel="stylesheet" href="assets/css/fontawesome.css">
-<link rel="stylesheet" href="assets/css/templatemo-sixteen.css">
+<link rel="stylesheet" href="assets/css/templatemo.css">
 <link rel="stylesheet" href="assets/css/owl.css">
 
 <link href="{{ asset('assets/css/fontawesome.css') }}" rel='stylesheet' type='text/css' />
-<link href="{{ asset('assets/css/templatemo-sixteen.css') }}" rel='stylesheet' type='text/css' />
+<link href="{{ asset('assets/css/templatemo.css') }}" rel='stylesheet' type='text/css' />
 <link href="{{ asset('assets/css/owl.css') }}" rel='stylesheet' type='text/css' />
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
